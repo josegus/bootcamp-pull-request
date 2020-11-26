7376eff260bc59b74f492c54bbc32630c8f869f4
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2020-11-25 20:18:35 -0400

    saludo santillan

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/santillan', function () {
+    echo 'Hola soy Santillan';
     return view('welcome');
 });
